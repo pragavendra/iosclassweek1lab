baceb1184ec5d3d47ace349b01eb2437c3a2cbaf
pragavendra <pragave@L-SB8M11KG8W-M.local>
2016-05-21 00:58:24 -0700

    Week1lab facebook challenge
    Submitting  the lab work for week 1

---
+++ iosclassweek1lab/SettingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.swift
+//  iosclassweek1lab
+//
+//  Created by Padmaja Ragavendra on 5/20/16.
+//  Copyright © 2016 Padmaja Ragavendra. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+     @IBOutlet weak var scrollView: UIScrollView!
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+weak var imageView: UIImageView!
+        // Do any additional setup after loading the view.
+    }
+
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
